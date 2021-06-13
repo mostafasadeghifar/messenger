0334f65a87d59ccb1292a53eedd7cc97df5fc306
mostafa sadeghifar <msadeghifar1996@gmail.com>
2021-06-13 16:14:58 +0430

    Initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Prophecy\Call\Call;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            ChatRoomSeeder::class
+        ]);
+        // \App\Models\User::factory(10)->create();
+    }
+}
